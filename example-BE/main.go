d1abff490974c4dac546d81bdb59c6585acb1a44
ChloeSVillaranda <chloesvillaranda@gmail.com>
2025-03-17 12:25:29 -0400

    example of websockets

--- example-BE/main.go
+++ example-BE/main.go
@@ -2,72 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
 
-var clients = make(map[*websocket.Conn]bool)
-var broadcast = make(chan Message)
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
 
-type Message struct {
-	Type string `json:"type"`
-	ID   string `json:"id"`
-}
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
+// WebSocket handler
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+	// Upgrade HTTP connection to WebSocket
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return
 	}
-	defer ws.Close()
+	defer conn.Close()
 
-	clients[ws] = true
-	id := fmt.Sprintf("%p", ws)
-
-	broadcast <- Message{Type: "new-client", ID: id}
-
-	for {
-		_, _, err := ws.ReadMessage()
-		if err != nil {
-			delete(clients, ws)
-			broadcast <- Message{Type: "client-left", ID: id}
-			break
-		}
+	// Wait for the client to send the client ID
+	_, clientID, err := conn.ReadMessage()
+	if err != nil {
+		log.Println(err)
+		return
 	}
-}
 
-func handleMessages() {
+	// Log the client ID (sent by the frontend)
+	log.Printf("%s connected\n", string(clientID))
+
+	// Listen for messages from the client
 	for {
-		msg := <-broadcast
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				fmt.Println(err)
-				client.Close()
-				delete(clients, client)
-			}
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
 		}
+		log.Printf("%s: %s\n", clientID, message)
 	}
 }
 
 func main() {
-	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
-
-	http.HandleFunc("/ws", handleConnections)
-
-	go handleMessages()
-
-	fmt.Println("Server started on :8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+	http.HandleFunc("/ws", handleConnection)
+	fmt.Println("WebSocket server running on ws://localhost:8080/ws")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- example-FE/src/routes/+page.svelte
+++ example-FE/src/routes/+page.svelte
@@ -1,7 +1,42 @@
 <script lang="ts">
-	import Client from "./Client.svelte";
+    import Client from "./Client.svelte";
 
 	let name = $state('world');
+
+	let clientID: string;
+
+	// Check if we already have a client ID in localStorage
+	if (!localStorage.getItem("clientID")) {
+		// If no client ID, assign a new one and store it
+		clientID = "client_" + Date.now();  // Unique ID based on the current timestamp
+		localStorage.setItem("clientID", clientID); // Store it in localStorage
+	} else {
+		// If we have a client ID in localStorage, use it
+		clientID = localStorage.getItem("clientID")!;
+	}
+
+	// This function is triggered when the button is clicked
+	function connectToServer() {
+		let socket = new WebSocket('ws://localhost:8080/ws');
+
+		socket.onopen = () => {
+			console.log('Connected to server as ' + clientID);
+			// Send the client ID to the server
+			socket.send(clientID);
+		};
+
+		socket.onmessage = (event) => {
+			console.log("Message from server:", event.data);
+		};
+
+		socket.onerror = (error) => {
+			console.error("WebSocket error:", error);
+		};
+
+		socket.onclose = () => {
+			console.log('Connection closed');
+		};
+	}
 
 </script>
 
@@ -11,6 +46,7 @@
 </svelte:head>
 
 <section>
+	<button on:click={connectToServer}>Connect to WebSocket Server</button>
 	
 	<input bind:value={name}>
 
@@ -48,4 +84,11 @@
 		top: 0;
 		display: block;
 	}
+
+	button {
+		padding: 10px 20px;
+		font-size: 16px;
+		cursor: pointer;
+	}
+
 </style>
