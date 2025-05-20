16e92010e87528f9d33cd8f22e7206d688c462c2
ChloeSVillaranda <chloesvillaranda@gmail.com>
2025-05-20 16:05:34 -0600

    updated the middleware

--- react-fe/src/stores/store.ts
+++ react-fe/src/stores/store.ts
@@ -1,9 +1,13 @@
+// redux middleware
+
 import { configureStore } from "@reduxjs/toolkit";
+import gameReducer from "./gameSlice";
 import websocketReducer from "./websocketSlice";
 
 const store = configureStore({
   reducer: {
     websocket: websocketReducer,
+    game: gameReducer, 
   },
 });
 
